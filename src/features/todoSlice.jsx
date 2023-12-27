e22603c916159ed3c13d6d6f20571cf139948bf8
kasahun welela <getasewmequanint@cbe.com.et>
2023-12-27 15:42:07 +0300

    create todo slice

---
+++ src/features/todoSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+const todoSlice = createSlice({
+  name: todo,
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.tasks.push({ id: Date.now(), text: action.payload });
+    },
+    deleteTodo: (state, action) => {
+      state.tasks.filter((task) => task.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
